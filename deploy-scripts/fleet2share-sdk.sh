43243326977b0f8280c242126d6bb247341d9ea2
william <jinwei@gmail.com>
2022-05-14 22:07:49 +0800

    add platform-services/vehicle-authority/virtual-car-wallet support(include deploy scripts, postman-collections and one click deploy script-deploy.sh) refactoring postman-collections

--- deploy-scripts/fleet2share-sdk.sh
+++ deploy-scripts/fleet2share-sdk.sh
@@ -2,4 +2,6 @@ git clone https://github.com/bloxmove-com/bloxmove-identity-sdk-public.git
 
 cd bloxmove-identity-sdk-public
 
+git pull
+
 ./build.sh
--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,10 @@ echo "0.all"
 echo "1.fleet-node"
 echo "2.TOMP-gateway"
 echo "3.build fleet2share SDK(identity-ionic-native-plugin)"
+echo "4.base-platform(hardhat & ipfs)"
+echo "5.platform-services"
+echo "6.vehicle-authority"
+echo "7.virtual-car-wallet"
 
 function number_prompt() {
   while true; do
@@ -15,6 +19,10 @@ function number_prompt() {
           1 ) ./deploy-scripts/fleet-node.sh; break;;
           2 ) ./deploy-scripts/tomp-gateway.sh; break;;
           3 ) ./deploy-scripts/fleet2share-sdk.sh; break;;
+          4 ) ./deploy-scripts/base-platform.sh; break;;
+          5 ) ./deploy-scripts/platform-services.sh; break;;
+          6 ) ./deploy-scripts/vehicle-authority.sh; break;;
+          7 ) ./deploy-scripts/virtual-car-wallet.sh; break;;
       esac
   done
 }
